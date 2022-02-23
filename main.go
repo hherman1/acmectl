87531d9d30579ef3fd01fbdc30c901f6197f46f0
Hunter Herman <hherman@palantir.com>
2022-02-22 17:50:42 -0800

    onexec support

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 
@@ -12,13 +13,13 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use: "acmectl",
-	Short:	"acmectl allows you to manipulate the acme editor easily from the shell",
+	Use:   "acmectl",
+	Short: "acmectl allows you to manipulate the acme editor easily from the shell",
 }
 
 var newCmd = &cobra.Command{
-	Use: "new",
-	Short: 	"new creates a new acme window and returns its ID",
+	Use:   "new",
+	Short: "new creates a new acme window and returns its ID",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		win, err := acme.New()
 		if err != nil {
@@ -31,9 +32,9 @@ var newCmd = &cobra.Command{
 }
 
 var ctlCmd = &cobra.Command{
-	Use: "ctl <winid> <ctl>",
-	Short: 	"ctl sends an acme control message to the given window",
-	Args: cobra.MinimumNArgs(2),
+	Use:   "ctl <winid> <ctl>",
+	Short: "ctl sends an acme control message to the given window",
+	Args:  cobra.MinimumNArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		id, err := strconv.Atoi(args[0])
 		if err != nil {
@@ -50,7 +51,7 @@ var ctlCmd = &cobra.Command{
 // Writes to the given file. Implements io.Writer
 type writer struct {
 	win *acme.Win
-	f string
+	f   string
 }
 
 func (w writer) Write(p []byte) (n int, err error) {
@@ -58,9 +59,9 @@ func (w writer) Write(p []byte) (n int, err error) {
 }
 
 var writeCmd = &cobra.Command{
-	Use: "write <winid> <winfile>",
-	Short: 	"write copies stdin to the window's winfile",
-	Args: cobra.ExactArgs(2),
+	Use:   "write <winid> <winfile>",
+	Short: "write copies stdin to the window's winfile",
+	Args:  cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		id, err := strconv.Atoi(args[0])
 		if err != nil {
@@ -76,7 +77,7 @@ var writeCmd = &cobra.Command{
 		}
 		dst := writer{
 			win: win,
-			f: args[1],
+			f:   args[1],
 		}
 		_, err = io.Copy(dst, os.Stdin)
 		if err != nil {
@@ -88,9 +89,9 @@ var writeCmd = &cobra.Command{
 }
 
 var readCmd = &cobra.Command{
-	Use: "read <winid> <winfile>",
-	Short: 	"read cats the entire contents of the given winfile",
-	Args: cobra.ExactArgs(2),
+	Use:   "read <winid> <winfile>",
+	Short: "read cats the entire contents of the given winfile",
+	Args:  cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		id, err := strconv.Atoi(args[0])
 		if err != nil {
@@ -113,8 +114,8 @@ var readCmd = &cobra.Command{
 }
 
 var lsCmd = &cobra.Command{
-	Use: "ls <winid>",
-	Short: 	"ls lists the available window files",
+	Use:   "ls <winid>",
+	Short: "ls lists the available window files",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println(`addr
 body
@@ -127,6 +128,45 @@ xdata`)
 	},
 }
 
+var onCmd = &cobra.Command{
+	Use:   "onexec <winid> <event> <command...>",
+	Short: "Intercepts matching execute events from  the given windows event channel. When found, runs the command. Exits when the window is closed.",
+	Args:  cobra.MinimumNArgs(3),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		id, err := strconv.Atoi(args[0])
+		if err != nil {
+			return fmt.Errorf("parse winid: %w", err)
+		}
+		ev := args[1]
+		cmdName := args[2]
+		cargs := args[3:]
+
+		win, err := acme.Open(id, nil)
+		if err != nil {
+			return fmt.Errorf("open window: %w", err)
+		}
+
+		for e := range win.EventChan() {
+			switch e.C2 {
+			case 'x', 'X': // execute
+				if string(e.Text) == ev {
+					exe := exec.Command(cmdName, cargs...)
+					exe.Stdout = os.Stdout
+					exe.Stderr = os.Stderr
+					err := exe.Run()
+					if err != nil {
+						fmt.Println(err)
+						continue
+					}
+					continue
+				}
+			}
+			win.WriteEvent(e)
+		}
+		return nil
+	},
+}
+
 func main() {
 	if err := run(); err != nil {
 		fmt.Println(err)
@@ -135,6 +175,6 @@ func main() {
 }
 
 func run() error {
-	rootCmd.AddCommand(newCmd, ctlCmd, writeCmd, readCmd, lsCmd)
+	rootCmd.AddCommand(newCmd, ctlCmd, writeCmd, readCmd, lsCmd, onCmd)
 	return rootCmd.Execute()
 }
